8e7cf912dec6b8b7878d6d4d3ae351956ecb6848
VegaMex <s16120245@alumnos.itsur.edu.mx>
2020-04-19 19:37:09 -0500

    Búsqueda de casa

--- MapaRuta/app/src/main/java/com/example/maparuta/MapasFragment.java
+++ MapaRuta/app/src/main/java/com/example/maparuta/MapasFragment.java
@@ -1,17 +1,29 @@
 package com.example.maparuta;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -19,6 +31,8 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -28,12 +42,16 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.maps.android.PolyUtil;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -56,6 +74,18 @@ public class MapasFragment extends Fragment implements OnMapReadyCallback {
     private String mParam2;
 
     private OnFragmentInteractionListener mListener;
+
+    // VARIABLES PROPIAS
+    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String COURSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
+    private static final float DEFAULT_ZOOM = 15f;
+
+    private GoogleMap mMapa;
+    private JSONObject mJsonObject;
+    private boolean mPermisos = false;
+    private EditText mBuscar;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
 
     public MapasFragment() {
         // Required empty public constructor
@@ -86,20 +116,15 @@ public class MapasFragment extends Fragment implements OnMapReadyCallback {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-
         View v = inflater.inflate(R.layout.fragment_mapas, container, false);
-
-
-        SupportMapFragment mapFragment = (SupportMapFragment) this.getChildFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-
-
+        mBuscar = v.findViewById(R.id.buscar);
+        pidePermisos();
         return v;
     }
 
@@ -127,73 +152,116 @@ public class MapasFragment extends Fragment implements OnMapReadyCallback {
         mListener = null;
     }
 
+    private void iniciarMapa() {
+        SupportMapFragment mapFragment = (SupportMapFragment) this.getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        inicializar();
+    }
 
-    GoogleMap map;
-    Boolean posicionActual=true;
-    JSONObject jso;
-    Double longitudOri, latitudOri;
+    private void inicializar() {
+        mBuscar.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH
+                        || actionId == EditorInfo.IME_ACTION_DONE
+                        || event.getAction() == event.ACTION_DOWN
+                        || event.getAction() == event.KEYCODE_ENTER) {
+                    localizar();
+                }
+                return false;
+            }
+        });
+    }
+
+    private void localizar() {
+        String busqueda = mBuscar.getText().toString();
+        Geocoder geocoder = new Geocoder(getActivity().getApplicationContext());
+        List<Address> lista = new ArrayList<>();
+        try {
+            lista = geocoder.getFromLocationName(busqueda, 1);
+        } catch (IOException e) {
+            Toast.makeText(getActivity().getApplicationContext(), "Algo pasó", Toast.LENGTH_SHORT).show();
+        }
+        if (lista.size() > 0) {
+            Address address = lista.get(0);
+            Toast.makeText(getActivity().getApplicationContext(), "Se trazó la ruta", Toast.LENGTH_SHORT).show();
+            Log.d("MENSAJES", address.toString());
+            tomarCoordenadas(address);
+        }
+    }
+
+    private void tomarCoordenadas(final Address address) {
+
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity().getApplicationContext());
+
+        try{
+            if(mPermisos){
+
+                final Task location = mFusedLocationProviderClient.getLastLocation();
+                location.addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if(task.isSuccessful()){
+                            Location currentLocation = (Location) task.getResult();
+
+                            moverCamara(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()),
+                                    DEFAULT_ZOOM);
+
+                            LatLng miPosicion = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+                            LatLng miCasa = new LatLng(address.getLatitude(), address.getLongitude());
+
+                            mMapa.addMarker(new MarkerOptions().position(miPosicion).title("Esta es mi posición actual"));
+                            mMapa.addMarker(new MarkerOptions().position(miCasa).title("Esta es mi casa"));
+
+                            String url = "https://maps.googleapis.com/maps/api/directions/json?origin=" + currentLocation.getLatitude() + "," + currentLocation.getLongitude() + "&destination="+ address.getLatitude() + "," + address.getLongitude() +"&key=AIzaSyC69im735dRrG4LtHyxduqr6bifbDSPOBY";
+
+                            RequestQueue queue = Volley.newRequestQueue(getActivity());
+                            StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+                                @Override
+                                public void onResponse(String response) {
+                                    try {
+                                        mJsonObject = new JSONObject(response);
+                                        trazarRuta(mJsonObject);
+                                        Log.i("jsonRuta: ", "" + response);
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            }, new Response.ErrorListener() {
+                                @Override
+                                public void onErrorResponse(VolleyError error) {
+                                }
+                            });
+                            queue.add(stringRequest);
+
+                        }else{
+                            Toast.makeText(getActivity(), "No se puede ir a tu casa :C", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        }catch (SecurityException e){
+            Toast.makeText(getActivity().getApplicationContext(), "Error de seguridad", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void moverCamara(LatLng latLng, float zoom){
+        mMapa.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+    }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
-        map = googleMap;
+        mMapa = googleMap;
 
-        map.setMyLocationEnabled(true);
-        map.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
-            @Override
-            public void onMyLocationChange(Location location) {
-
-
-                //20.0282742,-100.7256648
-
-                if (posicionActual){
-                    latitudOri = location.getLatitude();
-                    longitudOri = location.getLongitude();
-                    posicionActual=false;
-
-                    LatLng miPosicion = new LatLng(latitudOri,longitudOri);
-
-                    map.addMarker(new MarkerOptions().position(miPosicion).title("Aquí estoy"));
-
-                    CameraPosition cameraPosition = new CameraPosition.Builder()
-                            .target(new LatLng(latitudOri, longitudOri))
-                            .zoom(14)
-                            .bearing(30)
-                            .build();
-                    map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-
-
-                    String url = "https://maps.googleapis.com/maps/api/directions/json?origin="+latitudOri+","+longitudOri+"&destination=20.0223851,-100.7235959&key=AIzaSyC69im735dRrG4LtHyxduqr6bifbDSPOBY";
-
-                    RequestQueue queue= Volley.newRequestQueue(getActivity());
-                    StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
-                        @Override
-                        public void onResponse(String response) {
-
-                            try {
-                                jso = new JSONObject(response);
-                                trazarRuta(jso);
-                                Log.i("jsonRuta: ",""+response);
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-
-
-                        }
-                    }, new Response.ErrorListener() {
-                        @Override
-                        public void onErrorResponse(VolleyError error) {
-
-                        }
-                    });
-
-                    queue.add(stringRequest);
-                }
-
+        if (mPermisos) {
+            if (ActivityCompat.checkSelfPermission(getActivity().getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity().getApplicationContext(),
+                    Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                mMapa.setMyLocationEnabled(true);
+                return;
             }
-        });
-
-
+        }
     }
 
     private void trazarRuta(JSONObject jso) {
@@ -202,32 +270,73 @@ public class MapasFragment extends Fragment implements OnMapReadyCallback {
         JSONArray jLegs;
         JSONArray jSteps;
 
-        try{
-
+        try {
             jRoutes = jso.getJSONArray("routes");
-            for (int i=0; i<jRoutes.length(); i++){
-                jLegs = ((JSONObject)(jRoutes.get(i))).getJSONArray("legs");
+            for (int i = 0; i < jRoutes.length(); i++) {
+                jLegs = ((JSONObject) (jRoutes.get(i))).getJSONArray("legs");
 
-                for (int j = 0; j<jLegs.length(); j++){
+                for (int j = 0; j < jLegs.length(); j++) {
 
-                    jSteps = ((JSONObject)jLegs.get(j)).getJSONArray("steps");
+                    jSteps = ((JSONObject) jLegs.get(j)).getJSONArray("steps");
 
-                    for (int k = 0; k<jSteps.length(); k++){
+                    for (int k = 0; k < jSteps.length(); k++) {
 
-                        String polyline = ""+((JSONObject)((JSONObject)jSteps.get(k)).get("polyline")).get("points");
-                        Log.i("end", ""+polyline);
+                        String polyline = "" + ((JSONObject) ((JSONObject) jSteps.get(k)).get("polyline")).get("points");
+                        Log.i("end", "" + polyline);
                         List<LatLng> list = PolyUtil.decode(polyline);
-                        map.addPolyline(new PolylineOptions().addAll(list).color(Color.GRAY).width(5));
+                        mMapa.addPolyline(new PolylineOptions().addAll(list).color(Color.GRAY).width(5));
 
                     }
                 }
             }
-
-
         } catch (JSONException e) {
             e.printStackTrace();
         }
+    }
 
+    private void pidePermisos() {
+        String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION};
+
+        if (ContextCompat.checkSelfPermission(getActivity().getApplicationContext(),
+                FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(getActivity().getApplicationContext(),
+                    COURSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                mPermisos = true;
+                iniciarMapa();
+            } else {
+                ActivityCompat.requestPermissions(getActivity(),
+                        permissions,
+                        LOCATION_PERMISSION_REQUEST_CODE);
+            }
+        } else {
+            ActivityCompat.requestPermissions(getActivity(),
+                    permissions,
+                    LOCATION_PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        mPermisos = false;
+
+        switch (requestCode) {
+            case LOCATION_PERMISSION_REQUEST_CODE: {
+                if (grantResults.length > 0) {
+                    for (int i = 0; i < grantResults.length; i++) {
+                        if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                            mPermisos = false;
+                            Toast.makeText(getActivity().getApplicationContext(), "No hay permisos", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                    }
+                    Toast.makeText(getActivity().getApplicationContext(), "Permisos concedidos", Toast.LENGTH_SHORT).show();
+                    mPermisos = true;
+                    iniciarMapa();
+                }
+            }
+        }
     }
 
     /**
